103b757e91c63575ecce62735c8ce30c2a9fb175
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-11-19 13:11:05 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,6 +2,8 @@ class Transfer
   attr_accessor :sender, :receiver, :amount
   
   def initialize(sender, receiver, amount)
-    
+    @sender = sender 
+    @receiver = receiver 
+    @amount = amount
   end 
 end
